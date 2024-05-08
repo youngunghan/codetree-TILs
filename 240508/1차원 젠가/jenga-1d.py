79892defdd1aebd456ce4aa9fcd7c67ee773e08b
Youngung Han <hyulives@gmail.com>
2024-05-08 23:11:30 +0900

    Title: 1차원 젠가, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240508/1차원 젠가/jenga-1d.py
+++ 240508/1차원 젠가/jenga-1d.py
@@ -5,5 +5,20 @@ n = int(input())
 jenga = [int(input()) for _ in range(n)]
 s1, e1 = list(map(int, input().split()))
 s2, e2 = list(map(int, input().split()))
+print(jenga)
+
+del jenga[-e1:-s1]
+print(jenga)
+del jenga[-e2:-s2]
+# if s1 == e1:
+#     del jenga[s1:e1]  
+# else: 
+#     del jenga[s1:e1+1]
+
+# print(jenga)
+# if s2 == e2:
+#     del jenga[s2:e2]  
+# else: 
+#     del jenga[s2:e2+1]
 
 print(jenga)
